fba337e923de1a3c081b05c74e3f1b04fc6b6c17
reyjinn-IT <raihanpermadi17@gmail.com>
2026-09-23 04:52:41 +0700

    fix(database): polyfill BigInt serialization globally

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -3,4 +3,11 @@
  * © 2026 @reyjinnn
  * This project is exclusively owned by @reyjinnn.
  */
+// Polyfill BigInt serialization globally
+if (typeof BigInt.prototype !== "undefined") {
+  (BigInt.prototype as any).toJSON = function () {
+    return this.toString();
+  };
+}
+
 export * from "@prisma/client";
